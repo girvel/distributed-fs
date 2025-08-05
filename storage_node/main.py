b83612a364de9f25c39238e2a004e62b50233866
girvel <widauka@ya.ru>
2025-08-05 13:12:29 +0500

    GET file

--- storage_node/main.py
+++ storage_node/main.py
@@ -1,8 +1,33 @@
-from fastapi import FastAPI
+import os
+from pathlib import Path
+from fastapi import FastAPI, HTTPException, Response, status
+from starlette.responses import FileResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/ping")
-async def ping():
-    return {"pong": "pong"}
+# TODO env configuration
+FS_BASE_PATH = Path("./fs/")
+
+class DirectoryListing(BaseModel):
+    items: list[str]
+
+# TODO list directories
+# TODO test for paths such as ../../etc/passwd
+@app.get("/{file_path:path}", response_model=DirectoryListing | None)
+async def get_file(file_path: str, response: Response):
+    full_path = FS_BASE_PATH / file_path
+    if not full_path.exists():
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+    if full_path.is_file():
+        response.headers["X-Item-Type"] = "file"  # TODO document header
+        return FileResponse(full_path)
+
+    response.headers["X-Item-Type"] = "directory"
+    return {
+        "items": os.listdir(full_path),
+        # TODO also return is it file/directory
+        # TODO cache response
+    }
 
