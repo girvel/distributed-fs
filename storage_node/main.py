4e0f996d5e4d2739feb6f3a3c367d4140949ef3d
girvel <widauka@ya.ru>
2025-08-05 13:30:31 +0500

    storage_node: Uploading files

--- storage_node/main.py
+++ storage_node/main.py
@@ -1,24 +1,21 @@
 import os
 from pathlib import Path
-from fastapi import FastAPI, HTTPException, Response, status
+from fastapi import FastAPI, HTTPException, Response, UploadFile, status
 from starlette.responses import FileResponse
-from pydantic import BaseModel
 
 app = FastAPI()
 
 # TODO env configuration
-FS_BASE_PATH = Path("./fs/")
+FS_BASE_PATH = Path(".fs")
+CHUNK_SIZE = 8192
 
-class DirectoryListing(BaseModel):
-    items: list[str]
-
-# TODO list directories
+# TODO consider separate route for listing a directory, return zipped directory instead
 # TODO test for paths such as ../../etc/passwd
-@app.get("/{file_path:path}", response_model=DirectoryListing | None)
+@app.get("/{file_path:path}")
 async def get_file(file_path: str, response: Response):
     full_path = FS_BASE_PATH / file_path
     if not full_path.exists():
-        raise HTTPException(status.HTTP_404_NOT_FOUND)
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="File does not exist")
 
     if full_path.is_file():
         response.headers["X-Item-Type"] = "file"  # TODO document header
@@ -30,4 +27,15 @@ async def get_file(file_path: str, response: Response):
         # TODO also return is it file/directory
         # TODO cache response
     }
+
+@app.put("/{file_path:path}")
+async def set_file(file_path: str, file: UploadFile):
+    full_path = FS_BASE_PATH / file_path
+    full_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with open(full_path, "wb") as buffer:
+        while chunk := await file.read(CHUNK_SIZE):
+            _ = buffer.write(chunk)
+
+    return {"status": "success"}
 
